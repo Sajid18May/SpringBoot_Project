29a95c5b6b9512d3f491dbccc741c287d2e5d676
Sajid18May <sajid1852002@gmail.com>
2025-07-03 23:48:54 +0530

    added jstl support

--- src/main/java/com/employeemanagament/EmployeemanagementApplication.java
+++ src/main/java/com/employeemanagament/EmployeemanagementApplication.java
@@ -2,12 +2,19 @@ package com.employeemanagament;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeemanagementApplication {
+public class EmployeemanagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagementApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmployeemanagementApplication.class);
+    }
 
 }
--- src/main/webapp/WEB-INF/views/Employee.jsp
+++ src/main/webapp/WEB-INF/views/Employee.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ include file="header.jsp" %>
+<%@ taglib prefix="e" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +11,7 @@
 </head>
 <body>
 <h2>Employee Registration Page ...</h2>
+<e:out value="Hello"></e:out>
 	<form action="saveEmp" method="post">
 		<pre>
 			First Name<input type="text" name="firstName">
