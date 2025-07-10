2a4c64b8f8a5dca02ae8d361b2cd25316aae25d6
Sajid18May <sajid1852002@gmail.com>
2025-07-10 21:42:15 +0530

    comming extra changes

--- src/main/java/com/employeemanagament/service/EmployeeService.java
+++ src/main/java/com/employeemanagament/service/EmployeeService.java
@@ -2,10 +2,14 @@ package com.employeemanagament.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.employeemanagament.dto.EmployeeDto;
+import com.employeemanagament.entity.Address;
 import com.employeemanagament.entity.Employee;
+import com.employeemanagament.repository.AddressRepository;
 import com.employeemanagament.repository.EmployeeRepository;
 
 @Service
@@ -13,8 +17,27 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository emprepo;
 	
+	@Autowired
+	private AddressRepository addressrepo;
+	
 	public void saveEmployee(Employee employee) {
 		emprepo.save(employee);
+	}
+	
+	public EmployeeDto savaEmployeeDto(EmployeeDto employeeDto) {
+		Employee emp=new Employee();
+		BeanUtils.copyProperties(employeeDto, emp);
+		Address add=new Address();
+		BeanUtils.copyProperties(employeeDto, add);
+		
+		Employee employeeSaved = emprepo.save(emp);
+		Address addredSaved=addressrepo.save(add);
+		
+		EmployeeDto dto = new EmployeeDto();
+		BeanUtils.copyProperties(employeeSaved, dto);
+		BeanUtils.copyProperties(addredSaved, dto);
+		
+		return dto;
 	}
 	
 	public List<Employee> getAllEmployees(){
